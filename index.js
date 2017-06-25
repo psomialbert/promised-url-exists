1291f5ad4ae0d1350a430fd68a0fcabd5fb889f4
Albert Schapiro <albert.schapiro@getdreams.com>
2017-06-25 19:22:15 +0200

    Updated tests and fixed bug where it never resolved with `false` when url was missing.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var request = require('request-promise-native');
+var rp = require('request-promise-native');
 
 module.exports = function(url) {
-  return new Promise(function(resolve, reject) {
-    request({ url: url, method: 'HEAD' })
+  return new Promise((resolve, reject) => {
+    rp({ url: url, method: 'HEAD' })
       .then(result => {
         resolve(/4\d\d/.test(result.statusCode) === false);
       })
-      .catch(error => {
-        reject(error);
+      .catch(reason => {
+        if (reason.error.code === 'ENOTFOUND') {
+          resolve(false);
+        } else {
+          reject(error);
+        }
       });
   });
 };
--- test/index.js
+++ test/index.js
@@ -2,21 +2,28 @@ var assert = require('assert');
 
 var urlExists = require('..');
 
-urlExists('https://www.google.com', function(err, exists) {
-  assert(exists === true);
-
-  urlExists('https://google.com', function(err, exists) {
+urlExists('https://www.google.com')
+  .then(exists => {
     assert(exists === true);
 
-    urlExists('http://www.google.com', function(err, exists) {
-      assert(exists === true);
+    urlExists('https://www.google.com')
+      .then(exists => {
+        assert(exists === true);
 
-      urlExists('https://www.asdflkasdfljalsfdjasfdljklsjafasdfljjkasfdsafdljfdsaljakljsdljksafasfdlk.lasjkd', function(err, exists) {
-        assert(exists === false);
-
-        console.log('All tests pass!');
-        process.exit(0);
+        urlExists('https://www.gasdasdoogle.com')
+          .then(exists => {
+            assert(exists === false);
+            console.log('All tests pass!');
+            process.exit(0);
+          })
+          .catch(reason => {
+            assert(false);
+          });
+      })
+      .catch(reason => {
+        assert(false);
       });
-    });
+  })
+  .catch(reason => {
+    assert(false);
   });
-});
